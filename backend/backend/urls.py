af7e9cf5f9a95d64970a1d2d8d803be01c346dd2
2433528 <2433528@alu.murciaeduca.es>
2026-03-11 18:38:26 +0100

    inicio

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('inicio.urls')),
+    path('inicio/', include('inicio.urls')),
+    path('', RedirectView.as_view(url='/inicio/', permanent=True)),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
