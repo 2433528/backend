bdad091cd5b746bf527af93c6b4a5d092d734431
2433528 <2433528@alu.murciaeduca.es>
2026-03-11 18:10:13 +0100

    preparado para despliegue

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,10 +27,14 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'cambiame')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['.vercel.app', 'localhost', '127.0.0.1']
 
 CORS_ALLOW_ALL_ORIGINS = True
-#CORS_ALLOWED_ORIGINS = ['*']
+
+#CORS_ALLOWED_ORIGINS = [
+    #"https://tu-frontend.vercel.app",
+    #"http://localhost:3000",
+#]
 
 # Application definition
 
@@ -92,7 +96,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'backend.wsgi.app'
 
 
 # Database
@@ -145,7 +149,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = "/app/staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
